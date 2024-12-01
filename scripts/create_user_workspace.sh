f1735b0949732dbe6b5125fe01c20697456fecb5
Jai Bhagwanji <nik@gmail.com>
2024-12-01 19:24:32 +1100

    Updated user files

--- scripts/create_user_workspace.sh
+++ scripts/create_user_workspace.sh
@@ -9,7 +9,7 @@ TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USER}.tfvars"
 pwd
 ls -ltr
 
-terraform init -reconfigure -backend-config="prefix=user_states/$USER" || exit 1
+terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
 terraform plan -var-file="$TFVARS_FILE" || exit 1
 terraform apply -var-file="$TFVARS_FILE" -auto-approve || exit 1
 
