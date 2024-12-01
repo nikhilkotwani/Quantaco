f13965a6fd167abc4c849ea35f4fc624890d0f35
Jai Bhagwanji <nik@gmail.com>
2024-12-01 21:37:19 +1100

    Updated user files

--- scripts/delete_user_workspace.sh
+++ scripts/delete_user_workspace.sh
@@ -5,11 +5,15 @@ USER=$1
 # Initialize and apply Terraform for the user
 CURRENT_DIR=$(pwd)
 cd terraform
+STATE_LIST=$(terraform state list | grep 'google_storage_bucket.user_bucket')
 TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USER}.tfvars"
 pwd
 ls -ltr
 
-terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
-terraform plan -var-file="$TFVARS_FILE" || echo "No resources to destroy for $USER"
-terraform destroy -var-file="$TFVARS_FILE" -auto-approve || echo "No resources to destroy for $USER"
-
+for RESOURCE in $STATE_LIST; do
+  USER=$(echo "$RESOURCE" | sed -E 's/.*\["(.*)"\]/\1/')
+  if [ ! -f "$TFVARS_FILE" ]; then
+    echo "No .tfvars file found for $USER. Removing state for $RESOURCE."
+    terraform state rm "$RESOURCE" || exit 1
+  fi
+done
