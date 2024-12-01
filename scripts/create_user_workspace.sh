736bab2216961f88761fd357c5aed8346ef7cac8
Jai Bhagwanji <nik@gmail.com>
2024-12-01 19:08:42 +1100

    Updated user files

--- scripts/create_user_workspace.sh
+++ scripts/create_user_workspace.sh
@@ -5,6 +5,8 @@ VARS_FILE=$2
 
 # Initialize and apply Terraform for the user
 cd terraform
+pwd
+ls -ltr
 terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
 terraform plan -var-file="$VARS_FILE" || exit 1
 terraform apply -var-file="$VARS_FILE" -auto-approve || exit 1
