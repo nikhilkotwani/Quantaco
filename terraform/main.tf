ac406b958deac4019f93d03289750197d3b0e810
Jai Bhagwanji <nik@gmail.com>
2024-12-06 09:09:42 +1100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,5 @@ provider "google" {
 resource "google_storage_bucket" "user_bucket" {
   name     = "${var.bucket_prefix}-${var.user_name}"
   location = var.region
-  lifecycle_rule {
-    condition {
-      age = var.lifecycle_age
-    }
-    action {
-      type = "Delete"
-    }
-  }
   force_destroy = true
 }
