214d1be5ba8f6d565dc4c56536d8a7c139845a7d
Jai Bhagwanji <nik@gmail.com>
2024-12-06 07:18:25 +1100

    Trying to delete buckets based on state files not found

--- scripts/create_user_workspace.sh
+++ scripts/create_user_workspace.sh
@@ -10,19 +10,18 @@ cd terraform
 TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USER}.tfvars"
 pwd
 ls -ltr
-terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
-#terraform workspace select ${USER} || terraform workspace new ${USER}
-terraform plan -var-file="$TFVARS_FILE" || exit 1
 
 while [ -f "$LOCK_FILE" ]; do
     echo "Another job is running terraform apply. Waiting..."
-    sleep 5
+    sleep 20
 done
 
 # Create the lock file
 touch "$LOCK_FILE"
 
-
+terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
+#terraform workspace select ${USER} || terraform workspace new ${USER}
+terraform plan -var-file="$TFVARS_FILE" || exit 1
 terraform apply -var-file="$TFVARS_FILE" -auto-approve || exit 1
 
 rm -f "$LOCK_FILE"
--- scripts/delete_user_workspace.sh
+++ scripts/delete_user_workspace.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 USER=$1
+LOCK_FILE="/tmp/terraform-apply.lock"
+
 
 # Initialize and apply Terraform for the user
 CURRENT_DIR=$(pwd)
 cd terraform
-STATE_LIST=$(terraform state list | grep 'google_storage_bucket.user_bucket')
-echo $STATE_LIST
 TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USER}.tfvars"
 pwd
 ls -ltr
-echo $STATE_LIST
+terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
+#terraform workspace select ${USER} || terraform workspace new ${USER}
+terraform plan -var-file="$TFVARS_FILE" || exit 1
 
-for RESOURCE in $STATE_LIST; do
-  USER=$(echo "$RESOURCE" | sed -E 's/.*\["(.*)"\]/\1/')
-  if [ ! -f "$TFVARS_FILE" ]; then
-    echo "No .tfvars file found for $USER. Removing state for $RESOURCE."
-    terraform state rm "$RESOURCE" || exit 1
-  fi
+while [ -f "$LOCK_FILE" ]; do
+    echo "Another job is running terraform apply. Waiting..."
+    sleep 20
 done
+
+# Create the lock file
+touch "$LOCK_FILE"
+
+
+terraform apply -var-file="$TFVARS_FILE" -auto-approve || exit 1
+
+rm -f "$LOCK_FILE"
+
