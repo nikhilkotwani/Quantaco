ab079da97bf8e0b2fbc7e8fd3a71bc85e54bc2c1
Jai Bhagwanji <nik@gmail.com>
2024-12-06 10:00:33 +1100

    fix

--- scripts/create_user_workspace.sh
+++ scripts/create_user_workspace.sh
@@ -9,7 +9,7 @@ TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USERIP}.tfvars"
 pwd
 ls -ltr
 
-Check if lock exists
+# Check if lock exists
 # for user_file in terraform/user_vars/*.tfvars; do
 DIRECTORY="${CURRENT_DIR}/terraform/user_vars"
 find "$DIRECTORY" -type f | while read -r file; do
