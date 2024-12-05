324d30304f3194b35552ac87928c8d4f4bf9931b
Jai Bhagwanji <nik@gmail.com>
2024-12-06 08:37:46 +1100

    fix

--- scripts/create_user_workspace.sh
+++ scripts/create_user_workspace.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-USER=$1
+USERIP=$1
+$TFSTATE_BUCKET=$2
+
+
 
 
 # Initialize and apply Terraform for the user
 CURRENT_DIR=$(pwd)
 cd terraform
-TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USER}.tfvars"
+TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USERIP}.tfvars"
 pwd
 ls -ltr
 
-if [ -f "$LOCK_FILE" ]; then
-    echo "Another job is running terraform apply. Waiting..."
-    sleep 20
-fi
+# Check if lock exists
+
+for user_file in terraform/user_vars/*.tfvars; do
+    USER=$(basename "$user_file" .tfvars)
+    PREFIX="user_states/${USER}"
+    LOCK_OBJECT="${PREFIX}/default.tflock"
+    if gsutil stat "gs://${TFSTATE_BUCKET}/${LOCK_OBJECT}" &>/dev/null; then
+        echo "Lock exists for a user state file at ${LOCK_OBJECT}."
+        sleep 10
+    fi
+done
 
 terraform init -reconfigure \
--backend-config="prefix=user_states/${USER}" || exit 1
+-backend-config="prefix=${PREFIX}" || exit 1
 
 terraform plan -var-file="$TFVARS_FILE" || exit 1
 terraform apply -var-file="$TFVARS_FILE" -auto-approve || exit 1
--- scripts/generate_users.sh
+++ scripts/generate_users.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+$TFSTATE_BUCKET=$1
+
 # Generates user-specific Terraform commands
 for user_file in terraform/user_vars/*.tfvars; do
   user=$(basename "$user_file" .tfvars)
   echo "Processing $user..."
-  ./scripts/create_user_workspace.sh "$user"
+  ./scripts/create_user_workspace.sh "$user" "$TFSTATE_BUCKET"
 done
