03156086aa4460999a3575c0c8c2a502fa2f4178
Jai Bhagwanji <nik@gmail.com>
2024-12-06 08:41:46 +1100

    fix

--- scripts/generate_users.sh
+++ scripts/generate_users.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-$TFSTATE_BUCKET=$1
-
 # Generates user-specific Terraform commands
 for user_file in terraform/user_vars/*.tfvars; do
   user=$(basename "$user_file" .tfvars)
   echo "Processing $user..."
-  ./scripts/create_user_workspace.sh "$user" "$TFSTATE_BUCKET"
+  ./scripts/create_user_workspace.sh "$user"
 done
