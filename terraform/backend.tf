f92df46dace4d024b8823d8f0912c0534c7ba9c0
Jai Bhagwanji <nik@gmail.com>
2024-12-06 08:03:22 +1100

    Trying to delete buckets based on state files not found

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,8 @@
-# terraform {
-#   backend "gcs" {
-#     bucket  = "terraform-state-bucket-2024-abcd"
-#     prefix  = "user_states/"
-#   }
-# }
 terraform {
-  backend "gcs" {}
+  backend "gcs" {
+    bucket  = "terraform-state-bucket-2024-abcd"
+    prefix  = "user_states/"
+  }
 }
+
 
