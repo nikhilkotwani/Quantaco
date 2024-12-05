2b334ac7e8010b08a11246762076397df6a7e02e
Jai Bhagwanji <nik@gmail.com>
2024-12-06 06:43:54 +1100

    Trying to delete buckets based on state files not found

--- scripts/create_user_workspace.sh
+++ scripts/create_user_workspace.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 USER=$1
+LOCK_FILE="/tmp/terraform-apply.lock"
+
 
 # Initialize and apply Terraform for the user
 CURRENT_DIR=$(pwd)
@@ -8,8 +10,20 @@ cd terraform
 TFVARS_FILE="${CURRENT_DIR}/terraform/user_vars/${USER}.tfvars"
 pwd
 ls -ltr
-
 terraform init -reconfigure -backend-config="prefix=user_states/${USER}" || exit 1
+terraform workspace select ${USER} || terraform workspace new ${USER}
 terraform plan -var-file="$TFVARS_FILE" || exit 1
+
+while [ -f "$LOCK_FILE" ]; do
+    echo "Another job is running terraform apply. Waiting..."
+    sleep 5
+done
+
+# Create the lock file
+touch "$LOCK_FILE"
+
+
 terraform apply -var-file="$TFVARS_FILE" -auto-approve || exit 1
+
+rm -f "$LOCK_FILE"
 
